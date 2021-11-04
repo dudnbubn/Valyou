0957a901e6f733963ca71c56e86726e4ac9936dd
NaRae-tech <narae.dev@gmail.com>
2021-11-04 14:56:06 +0900

    login function making

--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -1,24 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "../css/Login.css";
 
-class Login extends React.Component {
-    render() {
-        return (
-            <div className="main__wrap">
-                <div className="login__wrap">
-                    <div className="login__id">
-                        <span>아이디 : </span>
-                        <input className="login__id-input" type="text" ></input>
-                    </div>
-                    <div className="login__pwd">
-                        <span>비밀번호 : </span>
-                        <input className="login__pwd-input" type="password"></input>
-                    </div>
-                    <button className="login__btn">로그인</button>
-                </div>
-            </div>
-        );
+function Login() {
+    const [inputId, setInputId] = useState('')
+    const [inputPw, setInputPw] = useState('')
+    const handleInputID = (e) => {
+        setInputId(e.target.value)
     }
+    const handleInputPw = (e) => {
+        setInputPw(e.target.value)
+    }
+    const onClickLoginBtn = () => {
+        axios.post('/*id, 비밀번호 보낼 주소*/', null, {
+            params: {
+                'userId': inputId,
+                'userPw': inputPw
+            }
+        }).then(res => {
+            console.log(res);
+            console.log(res.data.userId);
+            console.log(res.data.msg);
+            if (res.data.userId === undefined) {
+                alert('입력하신 id가 일치하지 않습니다.')
+            } else if (res.data.userId === null) {
+                alert('입력하신 비밀번호가 일치하지 않습니다.')
+            } else if (res.data.userId === inputId) {
+                sessionStorage.setItem('userId', inputId);
+            }
+            document.location.href = '/';
+        }).catch()
+    }
+    useEffect(() => {
+        axios.get('').then(res=>console.log(res)).catch()
+    },[]);
+    return (
+        <div className="main__wrap">
+            <div className="login__wrap">
+                <div className="login__id__wrap">
+                    <label htmlFor="login__id">아이디(ID) : </label>
+                    <input type="text" name="login__id" value={ inputId} className="login__id-input" onChange={ handleInputID}></input>
+                </div>
+                <div className="login__pwd__wrap">
+                    <label htmlFor="login__pwd">비밀번호(Password) : </label>
+                    <input type="password" name="login__pwd" value={inputPw} className="login__pwd-input" onChange={handleInputPw}></input>
+                </div>
+                <button type="button" className="login__btn" onClick={onClickLoginBtn}>로그인</button>
+            </div>
+        </div>
+    );
+
 }
 
 export default Login;
